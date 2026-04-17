f2de817001fc68f4163210b0562f961c206efd5c
eshwar <eshwarprudhvi2005@gmail.com>
2026-04-17 16:26:23 +0530

    updated cors for netlify

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ connectDB();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://budget-based-ai-oufit-generator.netlify.app/"],
+    origin: ["http://localhost:5173","https://budget-based-ai-oufit-generator.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
